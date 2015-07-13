cde13888d7a4451fdc6034788da7ae905c9b4e40
tayyabhussain <tayyab.hussain@coeus-solutions.de>
2015-07-13 11:03:11 +0500

    setup project

---
+++ models/Country.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: coeus
+ * Date: 7/1/15
+ * Time: 3:53 PM
+ */
+
+namespace app\models;
+
+use yii\db\ActiveRecord;
+
+class Country extends ActiveRecord
+{
+
+}
