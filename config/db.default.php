756f4b9daa5d5cc50c9d587c37b4be31de46cf6f
tayyabhussain <tayyab.hussain@coeus-solutions.de>
2015-07-16 15:14:32 +0500

    delete db.php and add default file

---
+++ config/db.default.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname= DB Name',
+    'username' => '',
+    'password' => '',
+    'charset' => 'utf8',
+];
--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yiiTest',
-    'username' => 'root',
-    'password' => 'coeus123',
-    'charset' => 'utf8',
-];
