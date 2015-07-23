d27a73a7f8c46d4f81210f0a889fb9515e19ec93
tayyabhussain <tayyab.hussain@coeus-solutions.de>
2015-07-23 17:07:16 +0500

    update design

--- views/site/login.php
+++ views/site/login.php
@@ -30,7 +30,7 @@ $this->params['breadcrumbs'][] = $this->title;
             <?= $form->field($model, 'password')->passwordInput(['class' => 'field-height password-top-margin'])->label('Password', ['class' => 'label-right-margin']) ?>
         </div>
     </div>
-    <div class='margin-chk-box'>
+    <div class='checkBox-parent'>
         <?= $form->field($model, 'rememberMe', [
             'template' => "<div class=\"col-lg-offset-1 col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
         ])->checkbox() ?>
