5dd1d1783086cbf459d1f2ea8a09b7984faf735b
Abdullah Ahmed <itconflux@gmail.com>
2015-07-24 16:11:30 +0500

    Done some work on Login page styling

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -54,6 +54,8 @@ class SiteController extends Controller
 
     public function actionLogin()
     {
+        $this->layout = 'design';
+
         if (!\Yii::$app->user->isGuest) {
             return $this->goHome();
         }
