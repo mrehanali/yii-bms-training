5b68537bd408cac18cbbda85a665042fd04ad378
Abdullah Ahmed <itconflux@gmail.com>
2015-07-28 11:56:11 +0500

    added user seed for admin

--- migrations/m150728_052218_create_users_table.php
+++ migrations/m150728_052218_create_users_table.php
@@ -19,6 +19,18 @@ class m150728_052218_create_users_table extends Migration
             'created_at'=> Schema::TYPE_TIMESTAMP . ' NOT NULL DEFAULT CURRENT_TIMESTAMP',
             'updated_at'=> Schema::TYPE_TIMESTAMP . ' NOT NULL DEFAULT "0000-00-00 00:00:00"',
         ]);
+        $this->insert('users', [
+            'id' => '1',
+            'first_name' => 'Abdullah',
+            'last_name' => 'Ahmed',
+            'username' => 'admin',
+            'password' => Yii::$app->security->generatePasswordHash('admin'),
+            'email' => 'abdullah.ahmed@coeus-solutions.de',
+            'auth_key' => 'R@P1dc0d3r',
+            'password_reset_token' => 'R@P1dc0d3r',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => '0000-00-00 00:00:00',
+        ]);
     }
 
     public function down()
